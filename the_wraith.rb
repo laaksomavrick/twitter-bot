84f83a296a41c2626069ef5e78022775eb7c5f97
Mavrick Laakso <laakso.mavrick@gmail.com>
2015-08-08 13:36:28 -0400

    updated script for heroku free app usage

--- the_wraith.rb
+++ the_wraith.rb
@@ -2,22 +2,25 @@ require 'rubygems'
 require 'twitter'
 
 client = Twitter::REST::Client.new do |config|
-	config.consumer_key = ""
-	config.consumer_secret = ""
-	config.access_token = ""
-	config.access_token_secret = ""
+	config.consumer_key = "8tf2QybvvnLWGL3u2AjdCFymE"
+	config.consumer_secret = "v8NSQanbSqdwQcwRYCdo3ANEtpH63suoTM6eH1GY0MSTyFmtFa"
+	config.access_token = "3404803155-i9XwIvE3hkxsmjetwgPAJybGRcGqTaNIJsqTUrr"
+	config.access_token_secret = "bEGOObokAcB19uafDEKNyCaE4OvN4s58YQ6YJ3EfQIjJS"
 end
-
-#client.update("test")
-
 file = File.open("ij2.txt")
 contents = file.read
 contents = contents.split.join(' ')
 tweets = contents.scan(/.{137}/)
 
-tweets.each do |tweet|
-  client.update(tweet + "...")
-  sleep 3600
+#this is not ideal code in itself, but necessary given Heroku's sleep mode for
+#free apps. database.txt acts as a database for saving the progress of iteration
+#whether or not the app sleeps (thus losing progress, as in the previous iteration)
+#of this script
+for i in 0..tweets.length
+  iterator = File.read('database.txt').to_i
+  client.update(tweets[i])
+  i += 1
+  File.write('database.txt', i)	
 end
 
 client.update("as raining out of a low sky, and the tide was way out.")
