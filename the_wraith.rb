378aa4c0ed9f9ee4f091ce6089c3874eb06894af
Mavrick Laakso <laakso.mavrick@gmail.com>
2015-08-10 00:34:33 -0400

    heroku ready

--- the_wraith.rb
+++ the_wraith.rb
@@ -2,11 +2,13 @@ require 'rubygems'
 require 'twitter'
 
 client = Twitter::REST::Client.new do |config|
-	config.consumer_key = ""
-	config.consumer_secret = ""
-	config.access_token = ""
-	config.access_token_secret = ""
+	config.consumer_key = "8tf2QybvvnLWGL3u2AjdCFymE"
+	config.consumer_secret = "v8NSQanbSqdwQcwRYCdo3ANEtpH63suoTM6eH1GY0MSTyFmtFa"
+	config.access_token = "3404803155-i9XwIvE3hkxsmjetwgPAJybGRcGqTaNIJsqTUrr"
+	config.access_token_secret = "bEGOObokAcB19uafDEKNyCaE4OvN4s58YQ6YJ3EfQIjJS"
 end
+
+#get the array of twitter ready
 file = File.open("ij2.txt")
 contents = file.read
 contents = contents.split.join(' ')
@@ -15,7 +17,7 @@ tweets = contents.scan(/.{137}/)
 iteration = tweets.length
 
 iteration.times do
-	client.update(tweets.sample)
+	client.update(tweets.sample + "...")
 	sleep 14400
 end
 
